6c07c84e09714f9035779e16be782d78c075249a
Sahithik1 <65923417+Sahithik1@users.noreply.github.com>
2020-12-18 21:55:07 -0500

    frontend

---
+++ src/app/service/authService/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, Subject } from 'rxjs';
+import { tokens } from '../../models/token';
+@Injectable()
+export class AuthService {
+
+  Tokens: Observable<any>;
+  loggedIn = new Subject<boolean>();
+  private configUrl = 'http://104.236.20.160:5000'
+  constructor(private http: HttpClient) { }
+
+  register(user) {
+    return this.http.post(`${this.configUrl}/user/register`, user);
+  }
+
+  login(user): Observable<tokens> {
+    return this.http.post<tokens>(`${this.configUrl}/user/login`, user);
+  }
+
+  logout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    this.setUserLoggedIn(false);
+  }
+
+  setUserLoggedIn(value: boolean) {
+    this.loggedIn.next(value)
+  }
+
+  getUserLoggedIn(): Observable<boolean> {
+    return this.loggedIn;
+  }
+}
